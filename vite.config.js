39ff1b8113d2de93de83d1437dd78c3b24ad3220
Matias Perdomo <matiasperdomo26@gmail.com>
2026-03-05 23:41:00 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()], // para JSX automático
-  build: {
-    lib: { entry: 'src/index.js', formats: ['es'], fileName: 'index' },
-    rollupOptions: {
-      // externals para no bundlear React ni el runtime de JSX
-      external: ['react', 'react-dom', 'react/jsx-runtime', 'react-bootstrap'],
-    },
+  plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': '"production"',
   },
-});
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      formats: ['es'],
+      fileName: 'common-ui'
+    }
+  }
+})
