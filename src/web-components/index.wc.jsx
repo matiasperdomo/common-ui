2fec98d6d434a93a1f660900df56ff2589c38f3e
Matias Perdomo <matiasperdomo26@gmail.com>
2026-03-09 02:35:48 -0300

    feat: export toSafeHref y fromPublicUri en bundles CDN

--- src/web-components/index.wc.jsx
+++ src/web-components/index.wc.jsx
@@ -49,7 +49,7 @@ if (!customElements.get('abc-footer')) customElements.define('abc-footer', AppFo
 // Utilidades expuestas como window.CommonUI.xxx
 export { formatFechaLongEs, formatFechaLargoES } from '../utils/dates.js';
 export { getYouTubeId, getYouTubeEmbedUrl, getYouTubeThumbUrl } from '../utils/youtube.js';
-export { toAbsolute, toHref, normalizeDrupalFileUrl } from '../utils/drupal.js';
+export { toAbsolute, toHref, toSafeHref, fromPublicUri, normalizeDrupalFileUrl } from '../utils/drupal.js';
 export { buildApiUrl, setApiBase } from '../utils/api.js';
 export { ensureEncodeSans, ensureIconFontAwesome } from '../utils/fonts.js';
 export * from '../utils/slug.js';
--- src/web-components/utils.entry.js
+++ src/web-components/utils.entry.js
@@ -5,7 +5,7 @@ import '../globals.js';
 
 export { formatFechaLongEs, formatFechaLargoES } from '../utils/dates.js';
 export { getYouTubeId, getYouTubeEmbedUrl, getYouTubeThumbUrl } from '../utils/youtube.js';
-export { toAbsolute, toHref, normalizeDrupalFileUrl } from '../utils/drupal.js';
+export { toAbsolute, toHref, toSafeHref, fromPublicUri, normalizeDrupalFileUrl } from '../utils/drupal.js';
 export { buildApiUrl, setApiBase } from '../utils/api.js';
 export { ensureEncodeSans, ensureIconFontAwesome } from '../utils/fonts.js';
 export * from '../utils/slug.js';
